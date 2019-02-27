f60eb406f1239224419a845d98d1c12e8666de8b
Paul Tobin <tobster111@gmail.com>
2019-02-27 17:17:11 +0000

    Updated addProductToCart + removeProductFromCart

--- src/App.js
+++ src/App.js
@@ -19,12 +19,40 @@ class App extends Component {
 
   addProductToCart = product => {
     console.log('Adding product', product);
-    this.state.cart.push(product);
-    console.log(this.state.cart);
+
+    const updatedCart = [...this.state.cart];
+    const updatedItemIndex = updatedCart.findIndex(
+      item => item.id === product.id
+    );
+
+    if (updatedItemIndex < 0) {
+      updatedCart.push({ ...product, quantity: 1 });
+    } else {
+      const updatedItem = {
+        ...updatedCart[updatedItemIndex]
+      };
+      updatedItem.quantity++;
+      updatedCart[updatedItemIndex] = updatedItem;
+    }
+    this.setState({ cart: updatedCart });
   };
 
   removeProductFromCart = productId => {
-    console.log('Removing product with' + productId);
+    const updatedCart = [...this.state.cart];
+    const updatedItemIndex = updatedCart.findIndex(
+      item => item.id === productId
+    );
+
+    const updatedItem = {
+      ...updatedCart[updatedItemIndex]
+    };
+    updatedItem.quantity--;
+    if (updatedItem.quantity <= 0) {
+      updatedCart.splice(updatedItemIndex, 1);
+    } else {
+      updatedCart[updatedItemIndex] = updatedItem;
+    }
+    this.setState({ cart: updatedCart });
   };
 
   render() {
