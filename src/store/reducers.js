7899884d2dd4aaeaa1d0bb4ceace38c747710185
Paul Tobin <tobster111@gmail.com>
2019-03-03 10:37:06 +0000

    Updated react to 16.8 to use hooks

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -16,7 +16,6 @@ const shopReducer = (state = initialState, action) => {
   let updatedItemIndex;
   switch (action.type) {
     case ADD_PRODUCT_TO_CART:
-      
 
     case REMOVE_PRODUCT_FROM_CART:
       updatedCart = [...state.cart];
